424748a9a710b8c9a9cba36d0bd9ca2ef8a79575
Sharmistha <freebird.sky4@gmail.com>
2017-10-23 01:07:17 +0530

    hh

--- src/main/java/com/golmanproblems/Smallest2ndInUnsortedArray.java
+++ src/main/java/com/golmanproblems/Smallest2ndInUnsortedArray.java
@@ -3,7 +3,7 @@ package com.golmanproblems;
 public class Smallest2ndInUnsortedArray {
 
 	public static void main(String[] args) {
-		int arr[]={1,1};
+		int arr[]={1,2,7,9,0,4};
 		System.out.println(print2ndsmallest(arr));
 	}
 	private static String print2ndsmallest(int[] arr) {
@@ -11,7 +11,7 @@ public class Smallest2ndInUnsortedArray {
 		int secondmin=Integer.MAX_VALUE;
 		if(arr!=null && arr.length>0){
 
-			for(int i=1;i<arr.length;i++){
+			for(int i=0;i<arr.length;i++){
 				if(arr[i]<firstmin){
 					secondmin=firstmin;
 					firstmin=arr[i];
