38a300491b6795e912aaa1a470db844d97619f4d
Sharmistha <freebird.sky4@gmail.com>
2017-10-22 23:09:34 +0530

    s

--- src/main/java/com/golmanproblems/SumEqualToGivenNumber.java
+++ src/main/java/com/golmanproblems/SumEqualToGivenNumber.java
@@ -1,11 +1,37 @@
 package com.golmanproblems;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class SumEqualToGivenNumber {
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 5, 7, -1, 5 };
         int sum = 6;
         getPairsCount(arr, sum);
+        getPairsCountWithN_TC(arr, sum);
+	}
+
+	private static void getPairsCountWithN_TC(int[] arr, int sum) {
+		HashMap<Integer, Integer> h=new HashMap<>();
+		int count=0;
+		for(int i=0;i<arr.length;i++) {
+			if(h.containsKey(arr[i])) {
+				h.put(arr[i], h.get(arr[i])+1);
+			}
+			else {
+				h.put(arr[i],1);				
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			if(h.get(sum-arr[i])>0) {
+				count+=h.get(sum-arr[i]);
+			}
+			if(arr[i] == h.get(sum-arr[i])) {
+				count--;
+			}
+		}
+		System.out.println(count/2);
 	}
 
 	private static void getPairsCount(int[] arr, int sum) {
