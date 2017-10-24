caea275757626a574dee832881481caf235df66a
snag2 <snag2@BLRVIKR16.sapient.com>
2017-10-24 11:42:12 +0530

    a

--- src/main/java/com/golmanproblems/MinimumDistanceFromMiddleIndexOfTheWord.java
+++ src/main/java/com/golmanproblems/MinimumDistanceFromMiddleIndexOfTheWord.java
@@ -10,33 +10,58 @@ public class MinimumDistanceFromMiddleIndexOfTheWord {
 
 	}
 
-	private static char[] findMinDistanceBetween(String s, String w, String w1) {
+	private static String findMinDistanceBetween(String s, String w, String w1) {
 		String[] words=s.split("\\W");
 		int cursor=0;
 		int distance=Integer.MAX_VALUE;
 		int wIndex=-1;
 		int w1Index=-1;
+		int wc=-1;
+		int w1c=-1;
+		int minDistance = Integer.MAX_VALUE;
+		
 		int cursorrange=0;
 		for(int i=0;i<words.length;i++) {
 			if(words[i].equals(w)) {
 				wIndex = i;
-				cursor+= words[i].length()/2+1;
+				wc=cursor+ words[i].length()/2+1;
 			}
 			else if(words[i].equals(w1)) {
 				w1Index = i;
-				cursor+= words[i].length()/2+1;
+				w1c=cursor+ words[i].length()/2+1;
 			}
 			if(wIndex!=-1 && w1Index!=-1) {
 				int range = Math.abs(w1Index-wIndex);
 				if(distance>range) {
 					distance = range;
-					cursorrange = 
+					cursorrange = Math.abs(w1c-wc);
 				}
 			}
 			cursor+= words[i].length()+1;
 		}
-
-		return null;
+		/*for(String word:words){
+		    // for(int i=0;i<strArray.length;i++){
+		      
+		      if(word.equals(w)){
+		        wc = cursor + word.length()/2;        
+		      }
+		      
+		      if(word.equals(w1)){
+		        w1c = cursor + word.length()/2;
+		      }
+		      
+		      distance = w1c - wc;
+		      System.out.println(distance);
+		      if(distance<0) distance=distance*-1;      
+		      
+		      if(distance<minDistance){
+		        minDistance = distance;
+		      }
+		      
+		      cursor = cursor+word.length()+1;
+		         
+		    }*/
+		return "cursorrange="+cursorrange+" distance="+distance;
 	}
 
 }
